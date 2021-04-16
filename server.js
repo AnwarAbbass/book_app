62731b0f1cadbd2354067dd5d2108a8de0e40eca
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-04-16 23:20:44 +0300

    database

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
+const DATABASE_URL= process.env.DATABASE_URL;
 const NODE_ENV = process.env.NODE_ENV;
-const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
+const options = NODE_ENV === 'production' ? { connectionString:  DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
 const client = new pg.Client(options);
 
 const methodOverride = require('method-override');
